f4601e78af1af7aecaa37fc3baf393526f18eccf
diogotito <diogotitomarques@gmail.com>
2023-02-24 23:14:02 +0000

    Keep attempting day 10 in Ruby

--- day10/solution.rb
+++ day10/solution.rb
@@ -1,11 +1,11 @@
-require 'minitest/autorun'
-
 class CPU
-  attr_accessor :X, :cycles
+  attr_accessor :X, :cycles, :crt
 
   def initialize
     @X = 1
     @cycles = [@X]
+    @r = @c = 1
+    @crt = Array.new(6) { Array.new(40) { ' ' }}
   end
 
   def run(program)
@@ -21,18 +21,23 @@ class CPU
 
   def addx(n)
     @cycles << @X
-    crt
+    upate_crt
     @cycles << @X
     @X += n
   end
 
   def noop
     @cycles << @X
-    crt
+    upate_crt
   end
 
-  def crt
-    # TODO
+  def upate_crt
+    @crt[@r - 1][@c - 1] = (@c - @X).abs <= 1 ? '#' : '.'
+    @c += 1
+    if @c == 40
+      @c = 0
+      @r += 1
+    end
   end
 
   def current_cycle = @cycles.count
@@ -40,6 +45,12 @@ class CPU
   def X_at(cycle, *rest) = @cycles[cycle, *rest]
   def cycle_strength(n) = n * X_at(n)
 end
+
+$cpu = CPU.new
+$cpu.run(open('input_example.txt'))
+puts $cpu.crt.map(&:join).join ?\n
+
+require 'minitest/autorun'
 
 describe CPU do
   it 'gives a sum of 13140 for interesting singals' do
@@ -50,9 +61,7 @@ describe CPU do
   end
 
   it 'gives the right answer to part 1' do
-    cpu = CPU.new
-    cpu.run(open('input.txt'))
-    _([20, 60, 100, 140, 180, 220].sum { cpu.cycle_strength _1 })
+    _([20, 60, 100, 140, 180, 220].sum { $cpu.cycle_strength _1 })
       .must_equal 14560
   end
 end
