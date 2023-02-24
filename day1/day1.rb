d65f54494f008ac7dc87eb423f571f9434f29f35
diogotito <diogotitomarques@gmail.com>
2023-02-24 23:15:53 +0000

    [Conciseness] Enumerable's #sum can run a given block

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,9 +1,6 @@
-
-
 elfs = File.foreach('input.txt', chomp: true)
-  .chunk { _1.empty? && :_separator }
-  .map { _2.map(&:to_i).sum }
+           .chunk { _1.empty? && :_separator }
+           .map { _2.sum(&:to_i) }
 
 puts elfs.max
 puts elfs.max(3).sum
-
