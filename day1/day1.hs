42c84aae7ecf398f920e0e2c004a2755d9ce87b9
diogotito <diogotitomarques@gmail.com>
2023-02-25 02:48:59 +0000

    Run stylish-haskell on day1.hs

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,30 +1,28 @@
-import Data.List(sort)
-import Control.Monad(forM_)
+import           Control.Monad (forM_)
+import           Data.List     (sort)
 
 -- Slices a list of lines in groups separated by empty lines
 records :: [String] -> [[String]]
 records lines = records' lines [] []
   where
-    records' ("":ls) r rs = records' ls    [] (r:rs)
-    records'  (l:ls) r rs = records' ls (l:r) rs
-    records'      [] r rs = r:rs
-
+    records' ("":ls) r rs = records' ls [] (r : rs)
+    records' (l:ls) r rs  = records' ls (l : r) rs
+    records' [] r rs      = r : rs
 
 part1 :: String -> Int
 part1 input =
-    let groups = records . lines $ input
-    in maximum $ map (sum . map read) groups
+  let groups = records . lines $ input
+   in maximum $ map (sum . map read) groups
 
 part2 :: String -> Int
 part2 input =
-    let groups = records . lines $ input
-    in sum . take 3 . reverse . sort . map (sum . map read) $ groups
-
+  let groups = records . lines $ input
+   in sum . take 3 . reverse . sort . map (sum . map read) $ groups
 
 main :: IO ()
 main = do
-    input <- readFile "input.txt"
-    forM_ (zip [1, 2] [part1, part2]) $ \(i, fn) -> do
-        putStrLn $ "=== Part " ++ (show i) ++ " ==="
-        putStrLn . show . fn $ input
-        putStrLn ""
+  input <- readFile "input.txt"
+  forM_ (zip [1, 2] [part1, part2]) $ \(i, fn) -> do
+    putStrLn $ "=== Part " ++ (show i) ++ " ==="
+    putStrLn . show . fn $ input
+    putStrLn ""
--- day1/day1_Text.hs
+++ day1/day1_Text.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.List(sortBy)
-import Control.Monad(forM_)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+
+import           Control.Monad  (forM_)
+import           Data.List      (sortBy)
+import qualified Data.Text      as T
+import qualified Data.Text.IO   as TIO
 import qualified Data.Text.Read as TR
 
 part1 = maximum . map sum
@@ -10,9 +11,11 @@ part1 = maximum . map sum
 part2 = sum . take 3 . sortBy (flip compare) . map sum
 
 main = do
-    groups <- readGroups <$> TIO.readFile "input.txt"
-    putStrLn $ "Part 1: " ++ show (part1 groups)
-    putStrLn $ "Part 2: " ++ show (part2 groups)
+  groups <- readGroups <$> TIO.readFile "input.txt"
+  putStrLn $ "Part 1: " ++ show (part1 groups)
+  putStrLn $ "Part 2: " ++ show (part2 groups)
   where
     readGroups = map (map readInt . T.lines) . T.splitOn "\n\n"
-    readInt t = let (Right (n, _)) = TR.decimal t in n
+    readInt t =
+      let (Right (n, _)) = TR.decimal t
+       in n
